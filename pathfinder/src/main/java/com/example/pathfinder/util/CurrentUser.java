38f66cbeed63ff3e9edf1e8661ccb1183eed454c
borissofft <kolegata86@abv.bg>
2023-01-28 14:33:47 +0200

    Make users/register validations in PostMapping, get valid attributes and populate it in the form when redirect to GetMapping for second try to fill valid registration fields

--- pathfinder/src/main/java/com/example/pathfinder/util/CurrentUser.java
+++ pathfinder/src/main/java/com/example/pathfinder/util/CurrentUser.java
@@ -1,7 +1,32 @@
 package com.example.pathfinder.util;
 
-public class CurrentUser {
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
+@Component
+@SessionScope
+public class CurrentUser { // Use it to logg the user if he is registered
 
+    private Long id;
+    private String username;
 
+    public CurrentUser() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- pathfinder/src/main/java/com/example/pathfinder/web/UserController.java
+++ pathfinder/src/main/java/com/example/pathfinder/web/UserController.java
@@ -1,22 +1,48 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.binding.UserRegisterBindingModel;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    // Variant 2
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel() {
+        return new UserRegisterBindingModel();
+    }
+
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        // Variant 1
+//        if (!model.containsAttribute("userRegisterBindingModel")) { // If this comes from frontend for the first time
+//            model.addAttribute("userRegisterBindingModel", new UserRegisterBindingModel()); // Get this empty model to use it in fronted to avoid exceptions
+//        }
+
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerConfirm() {
-        // TODO
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel, // Validate BindingModel. After this class always have to be BindingResult
+                                  BindingResult bindingResult,                              // Get errors after validation if exist
+                                  RedirectAttributes redirectAttributes) {                  // Save the result from UserRegisterBindingModel
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes
+                    .addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel",
+                            bindingResult);
+            return "redirect:register"; // register is the name of the method! We have populated valid data(in the form) because of using th:field="" in register.html
+        }
         return "redirect:/";
     }
 
@@ -27,4 +53,4 @@ public class UserController {
 
 }
 
-//24:45
+//1:20:00
