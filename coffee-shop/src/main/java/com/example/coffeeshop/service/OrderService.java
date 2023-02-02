11ecc9f0c650f6db6d6224a21737b8ccdaaa66d0
borissofft <kolegata86@abv.bg>
2023-02-02 20:13:59 +0200

    HomeController and UserController initial implementation. Extract common parts from html files and make fragments/commons.html. Add namespace for thymeleaf in html files

---
+++ coffee-shop/src/main/java/com/example/coffeeshop/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.example.coffeeshop.service;
+
+import com.example.coffeeshop.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+
+}
