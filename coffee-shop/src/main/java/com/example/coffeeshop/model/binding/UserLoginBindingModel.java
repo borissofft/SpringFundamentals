cf82aae282bfb47b4d407b4e90e0a32a6d7ef1bb
borissofft <kolegata86@abv.bg>
2023-02-04 19:09:13 +0200

    Edited form for login.html to see how we can add message in BindingModel and show it in the view instead of the message in the form

--- coffee-shop/src/main/java/com/example/coffeeshop/model/binding/UserLoginBindingModel.java
+++ coffee-shop/src/main/java/com/example/coffeeshop/model/binding/UserLoginBindingModel.java
@@ -23,7 +23,7 @@ public class UserLoginBindingModel {
     }
 
     @NotBlank
-    @Size(min = 3)
+    @Size(min = 3, message = "From BindingModel: Password length must be more than 3 characters.")
     public String getPassword() {
         return password;
     }
