a044e69ccc9087add49160e1e5539147b9f5cfc6
borissofft <kolegata86@abv.bg>
2023-02-05 18:49:10 +0200

    We can list all routes in RouteController

--- pathfinder/src/main/java/com/example/pathfinder/service/RouteService.java
+++ pathfinder/src/main/java/com/example/pathfinder/service/RouteService.java
@@ -1,7 +1,10 @@
 package com.example.pathfinder.service;
 
+import com.example.pathfinder.model.view.RouteViewModel;
+
+import java.util.List;
+
 public interface RouteService {
-
-
+    List<RouteViewModel> findAllRoutesView();
 
 }
--- pathfinder/src/main/java/com/example/pathfinder/web/RouteController.java
+++ pathfinder/src/main/java/com/example/pathfinder/web/RouteController.java
@@ -1,16 +1,33 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.view.RouteViewModel;
+import com.example.pathfinder.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/routes")
 public class RouteController {
+    private final RouteService routeService;
 
-    @GetMapping("")
-    public String allRoutes() {
+
+    @Autowired
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    @GetMapping("/all")
+    public String allRoutes(Model model) {
+        List<RouteViewModel> routesViewList = this.routeService.findAllRoutesView();
+        model.addAttribute("routes", routesViewList);
         return "routes";
     }
+
+
 
 }
